a3636b5d1ab84ef17ab736a94e66cbce51826561
BETHWEL NELSON OTIENO <bethwelbruce@gmail.com>
2022-11-20 00:37:16 +0300

    Add files via upload

---
+++ Nesting.py
@@ -0,0 +1,12 @@
+def solution(S):
+    p=0
+    for i in range(0,len(S)):
+        if S[i]=='(':
+            p+=1
+        elif S[i]==')':
+            p-=1
+    if p!=0:
+        return 0
+    else:
+        return 1
+    pass
